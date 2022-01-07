12cb3e48a03c1c7dc402c582c3106cb0ea343cdd
a17 <m@a17.name>
2022-01-07 12:57:37 +0300

    first pic

--- src/components/view/Home.tsx
+++ src/components/view/Home.tsx
@@ -5,6 +5,7 @@ function Home() {
       style={{ height: 'calc(100vh - 64px)' }}
     >
       <div>
+        <img src="/reactswap.png" alt="ReactSwap"/>
         <div className="flex text-3xl flex-col m-10 text-left font-bold">
           <div>Decentralized Exchange</div>
           <div>Yield farms</div>
